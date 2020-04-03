61170ec0b0ec20342bc783ee1634d2f56f186a20
Malgorzata <mkrzek@googlemail.com>
2020-04-03 09:18:35 +0100

    feat: pass page info to pagination through items comp

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,16 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import Items from '../components/Items';
 
-const Home = () => (
-  <div>
-    <Items />
-  </div>
-);
+const Home = props => {
+  const {
+    query: { page },
+  } = props;
+  console.log('INNNNNNNN', props);
+  return (
+    <div>
+      <Items page={parseFloat(page) || 1} />
+    </div>
+  );
+};
 export default Home;
