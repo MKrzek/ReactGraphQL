2a6f09a6144e008bca660c0d2a21f4c911d44ef7
Malgorzata <mkrzek@googlemail.com>
2020-04-03 16:53:07 +0100

    feat: add conditional rendering of nav buttons depending on whether a user is signed in or not

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import User from './User';
 
 const Nav = () => (
-  <NavStyles>
-    <Link href="/items">
-      <a>shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-  </NavStyles>
+  <User>
+    {({ data: { me } }) => (
+      <NavStyles>
+        <Link href="/items">
+          <a>shop</a>
+        </Link>
+        {me && (
+          <>
+            <Link href="/sell">
+              <a>sell</a>
+            </Link>
+            <Link href="/orders">
+              <a>orders</a>
+            </Link>
+            <Link href="/me">
+              <a>Account</a>
+            </Link>
+          </>
+        )}
+        {!me && (
+          <Link href="/signup">
+            <a>sign in</a>
+          </Link>
+        )}
+      </NavStyles>
+    )}
+  </User>
 );
 
 export default Nav;
