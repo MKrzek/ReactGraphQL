b2665b25cdd09b1fd306c7dd40872ec03b064648
Malgorzata <mkrzek@googlemail.com>
2020-04-03 13:14:54 +0100

    feat: connected signup mut with onSubm

--- frontend/components/SignUp.js
+++ frontend/components/SignUp.js
@@ -4,6 +4,21 @@ import gql from 'graphql-tag';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
 
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    signup(email: $email, name: $name, password: $password) {
+      id
+      email
+      name
+      password
+    }
+  }
+`;
+
 export default class SignUp extends Component {
   state = {
     email: '',
@@ -18,42 +33,53 @@ export default class SignUp extends Component {
   render() {
     const { name, email, password } = this.state;
     return (
-      <Form>
-        <fieldset>
-          <h2>Sign Up for a page</h2>
-          <label htmlFor="email">
-            Email
-            <input
-              type="text"
-              name="email"
-              placeholder="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="name">
-            Name
-            <input
-              type="text"
-              name="name"
-              placeholder="name"
-              value={name}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="password">
-            Password
-            <input
-              type="text"
-              name="password"
-              placeholder="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-          </label>
-          <button type="submit">Sign Up!</button>
-        </fieldset>
-      </Form>
+      <Mutation mutation={SIGNUP_MUTATION} variables={this.state}>
+        {(signup, { error, loading }) => (
+          <Form
+            method="post"
+            onSubmit={async e => {
+              e.preventDefault();
+              await signup();
+            }}
+          >
+            <fieldset disabled={loading} aria-busy={loading}>
+              <h2>Sign Up for a page</h2>
+              <Error error={error} />
+              <label htmlFor="email">
+                Email
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="email"
+                  value={email}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="name">
+                Name
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="name"
+                  value={name}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="password">
+                Password
+                <input
+                  type="text"
+                  name="password"
+                  placeholder="password"
+                  value={password}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <button type="submit">Sign Up!</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
     );
   }
 }
