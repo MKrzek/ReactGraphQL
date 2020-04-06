377fd4223dcccdc61c5e57b81364ebf2fc01959d
Malgorzata <mkrzek@googlemail.com>
2020-04-06 14:05:22 +0100

    feat: add password reset mutations

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt= require('bcryptjs');
 const jwt = require('jsonwebtoken')
+const {randomBytes} = require ('crypto')
+const {promisify} =require('util')
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem(
@@ -63,11 +65,55 @@ const Mutations = {
     })
     return user
   },
+
   async signout(parent, args, ctx, info){
   console.log('CCCCCCCCCCC', ctx.response)
    await ctx.response.clearCookie('token')
    return{message: "You have been signed-out"}
+  },
+
+  async requestReset(parent, args, ctx, info){
+    const user = await ctx.db.query.user({where: {email: args.email }})
+    if(!user){
+    throw new Error (`No such user fount for the email ${args.email}`)
+    }
+    const resetToken= (await promisify(randomBytes)(17)).toString('hex');
+    const resetTokenExpiry = Date.now() + 3600000;
+     await ctx.db.mutation.updateUser({
+       where: {email: args.email},
+       data: {resetToken, resetTokenExpiry}
+    })
+    return {message: "Thanks!"}
+  },
+  async resetPassword(parent, args, ctx, info){
+    if(args.password!==args.confirmPassword){
+    throw new Error('Your passwords do not match')
+    }
+    const [user] = await ctx.db.query.users({where:
+              {resetToken: args.resetToken,
+              resetTokenExpiry_gte:Date.now() - 3600000}})
+
+    if(!user){
+    throw new Error("This token is either invalid or expired")
+    }
+    const password = await bcrypt.hash(args.password, 10)
+    const updatedUser = await ctx.db.mutation.updateUser({
+    where: {email: user.email},
+    data:{
+     password,
+     resetToken:null,
+     resetTokenExpiry: null
+    }
+    })
+    const token = jwt.sign({ userId: updatedUser.id}, process.env.APP_SECRET)
+    ctx.response.cookie('token', token, {
+         httpOnly: true,
+         maxAge: 1000 *24
+    })
+    return updatedUser
+
   }
+
 };
 
 module.exports = Mutations;
