e7046f1e58ff768da090e5d416eb9094ad258fa8
Malgorzata <mkrzek@googlemail.com>
2020-04-09 14:57:39 +0100

    feat: only users with permissions will be able to delete items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -36,9 +36,16 @@ const Mutations = {
   },
 
   async deleteItem(parent, args, ctx, info ){
+
     const where = {id: args.id}
-    const item = await ctx.db.query.item({where}, `{id, title}`)
-    return ctx.db.mutation.deleteItem({where}, info)
+    const item = await ctx.db.query.item({where}, `{id, title, user{id}}`)
+
+    const ownsItem = item.user.id === ctx.request.userId
+    const hasPermission = ctx.request.user.permissions.some(permission=>['ADMIN', 'ITEMDELETE'].includes(permission))
+     if(!ownsItem || !hasPermission) {
+     throw new Error("You don't have permission to delete this item" )
+     }
+    return ctx.db.mutation.deleteItem({ where}, info)
 
   },
 
