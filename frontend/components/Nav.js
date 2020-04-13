b04653cf306c1ecefa006b9f5ac4a893b86a2416
Malgorzata <mkrzek@googlemail.com>
2020-04-13 13:50:50 +0100

    feat: add cart  button to nav

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
+import { Mutation } from 'react-apollo';
+import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import SignOut from './SignOut';
@@ -23,6 +25,13 @@ const Nav = () => (
               <a>Account</a>
             </Link>
             <SignOut />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {toggleCart => (
+                <button type="button" onClick={toggleCart}>
+                  My Cart
+                </button>
+              )}
+            </Mutation>
           </>
         )}
         {!me && (
