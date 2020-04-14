65f1220e2a2a37dda3c11050d984bada08f6ca0b
MKrzek <mkrzek@googlemail.com>
2020-04-14 11:35:16 +0100

    feat: user mutation will wrap up cart mutation to provide info about existing cart for a logged in user

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -5,6 +5,8 @@ import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
 import SickButton from './styles/SickButton';
+import User from './User';
+import CartItem from './CartItem';
 
 export const LOCAL_STATE_QUERY = gql`
   query {
@@ -18,27 +20,43 @@ export const TOGGLE_CART_MUTATION = gql`
 `;
 
 const Cart = () => (
-  <Mutation mutation={TOGGLE_CART_MUTATION}>
-    {toggleCart => (
-      <Query query={LOCAL_STATE_QUERY}>
-        {({ data }) => (
-          <CartStyles open={data.cartOpen}>
-            <header>
-              <CloseButton onClick={toggleCart} title="close">
-                Close
-              </CloseButton>
-              <Supreme>Your cart</Supreme>
-              <p>You have ___ in you cart</p>
-            </header>
-            <footer>
-              <p>$10.10</p>
-              <SickButton>Checkout</SickButton>
-            </footer>
-          </CartStyles>
-        )}
-      </Query>
-    )}
-  </Mutation>
+  <User>
+    {({ data: { me } }) => {
+       console.log('mEEEE', me)
+      if (!me) return null;
+      return (
+        <Mutation mutation={TOGGLE_CART_MUTATION}>
+          {toggleCart => (
+            <Query query={LOCAL_STATE_QUERY}>
+              {({ data }) => (
+                <CartStyles open={data.cartOpen}>
+                  <header>
+                    <CloseButton onClick={toggleCart} title="close">
+                      Close
+                    </CloseButton>
+                    <Supreme>{me.name}'s cart</Supreme>
+                    <p>
+                      You have {me.cart.length} item
+                      {me.cart.length > 1 && 's'} in you cart
+                    </p>
+                  </header>
+                  <ul>
+                    {me.cart.map(item => (
+                      <CartItem key={item.id} cartItem={item} />
+                    ))}
+                  </ul>
+                  <footer>
+                    <p>$10.10</p>
+                    <SickButton>Checkout</SickButton>
+                  </footer>
+                </CartStyles>
+              )}
+            </Query>
+          )}
+        </Mutation>
+      );
+    }}
+  </User>
 );
 
 export default Cart;
