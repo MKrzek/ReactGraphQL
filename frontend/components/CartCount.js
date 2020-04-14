5113bfcfcfa47161d7583854a07e16aed8750714
MKrzek <mkrzek@googlemail.com>
2020-04-14 16:09:13 +0100

    feat: add animation to count dot

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -1,6 +1,31 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import React from 'react';
+
+const AnimationStyles = styled.span`
+  position: relative;
+  .count {
+    display: block;
+    position: relative;
+    transition: all 0.4s;
+    backface-visibility: hidden;
+  }
+  .count-enter {
+    transform: scale(4) rotateX(0.5turn);
+  }
+  .count-enter-active {
+    transform: rotateX(0);
+  }
+  .count-exit {
+    top: 0;
+    position: absolute;
+    transform: rotateX(0);
+  }
+  .count-exit-active {
+    transform: scale(4) rotateX(0.5turn);
+  }
+`;
 
 const Dot = styled.div`
   background: ${props => props.theme.green};
@@ -15,6 +40,20 @@ const Dot = styled.div`
   font-variant-numeric: tabular-nums;
 `;
 
-const CartCount = ({ count }) => <Dot>{count}</Dot>;
+const CartCount = ({ count }) => (
+  <AnimationStyles>
+    <TransitionGroup>
+      <CSSTransition
+        unmountOnExit
+        className="count"
+        classNames="count"
+        key={count}
+        timeout={{ enter: 400, exist: 400 }}
+      >
+        <Dot> {count} </Dot>
+      </CSSTransition>
+    </TransitionGroup>
+  </AnimationStyles>
+);
 
 export default CartCount;
