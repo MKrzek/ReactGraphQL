27e4d533b875592b7d99ff1c8709f5db0ce41cc3
MKrzek <mkrzek@googlemail.com>
2020-04-14 11:58:25 +0100

    feat: add styles to cart item

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -3,7 +3,20 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import formatMoney from '../lib/formatMoney';
 
-const CartItemStyles = styled.li``;
+const CartItemStyles = styled.li`
+  padding: 1rem 0;
+  border-bottom: 1px solid ${props => props.theme.lightgrey};
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto 1fr auto;
+  img {
+    margin-right: 10px;
+  }
+  p,
+  h3 {
+    margin: 0;
+  }
+`;
 
 const CartItem = ({ cartItem }) => {
   const {
