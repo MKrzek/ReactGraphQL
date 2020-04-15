95cbc322095fbb9cf0c2f6329671bc1495c1d762
MKrzek <mkrzek@googlemail.com>
2020-04-15 11:32:22 +0100

    feat: wired up searchbar to header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import styled from 'styled-components';
 import Nav from './Nav';
 import Cart from './Cart';
+import Search from './Search';
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
@@ -64,7 +65,7 @@ const Header = () => (
       <Nav />
     </div>
     <div className="sub-bar">
-      <p>Search</p>
+      <Search />
     </div>
     <Cart />
   </StyledHeader>
